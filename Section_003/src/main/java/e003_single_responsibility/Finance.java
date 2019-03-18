d3b623ea0d1ae73743f8f36d259475135e53287c
allenlcp <allen.decodemtl@gmail.com>
2019-03-18 19:30:06 -0400

    Renamed folder

--- Section_003/src/main/java/e003_single_responsibility/Finance.java
+++
@@ -1,8 +0,0 @@
-package e003_single_responsibility;
-
-public class Finance {
-    public double calcIncomeTaxForCurrentYear(Employee emp){
-        // income tax logic using employee passed in
-        return 0;
-    }
-}
--- Section_003/src/main/java/e003_single_responsibility/HRPromotions.java
+++
@@ -1,8 +0,0 @@
-package e003_single_responsibility;
-
-public class HRPromotions {
-    public boolean isPromotionDueThisYear(Employee emp){
-        // promotion logic implementation is using the employee info passed in
-        return true;
-    }
-}
