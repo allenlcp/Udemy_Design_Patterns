52a32212400850180375eae525d275c92727a658
allenlcp <allen.decodemtl@gmail.com>
2019-03-22 13:39:30 -0400

    housekeeping

---
+++ Section_022_command/src/main/java/e001_command/FileInvoker.java
@@ -0,0 +1,13 @@
+package e001_command;
+
+class FileInvoker {
+    public Command command;
+
+    public FileInvoker(Command c) {
+        command = c;
+    }
+
+    public void execute() {
+        command.execute();
+    }
+}
